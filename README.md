69c0554fd08663b7ce209019f02e5c09525ab10a
Daniel Yinanc <danielyinanc@users.noreply.github.com>
2016-06-30 09:01:40 -0400

    initial push

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# test
-Functional and Performance Testing
+# Krazy Ideas Test
+[![License](http://img.shields.io/:license-apache-blue.svg?style=flat-square)](http://www.apache.org/licenses/LICENSE-2.0.html)
+
+Functional and performance testing of Krazy Ideas. Cucumber will be the framework for functional
+testing while Jmeter will be used for performance testing
+
+## Structure
+src/cucumber folder will house feature descriptions as well as java implementations.
+
---
+++ src/cucumber/java/com/rainmakeross/krazyideas/BackendStepDefs.java
@@ -0,0 +1,26 @@
+package com.rainmakeross.krazyideas;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class BackendStepDefs {
+    @Given("^a user with id (\\d+) exists$")
+    public void aUserWithIdExists(int arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I request user object for id (\\d+)$")
+    public void iRequestUserObjectForId(int arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I see all details of the user in json format$")
+    public void iSeeAllDetailsOfTheUserInJsonFormat() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
